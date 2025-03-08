1e105b836cac2d5a7b386f157262c4e2b9c791df
Tristan Hill <tphwoodlands@gmail.com>
2025-03-08 17:46:43 +0000

    caddy updates

--- server.js
+++ server.js
@@ -89,6 +89,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.use((req, res) => {
     console.log('404 - Not Found:', req.url);
     res.status(404).send('Not Found');
