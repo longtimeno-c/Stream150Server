f8fc3556d0121645f2e38c35d0c1a325a4d733c1
Tristan Hill <tphwoodlands@gmail.com>
2025-03-08 14:12:50 +0000

    break into seperate files

---
+++ public/js/donations.js
@@ -0,0 +1,11 @@
+class DonationTracker {
+    constructor() {
+        this.createMilestoneMarkers();
+        this.startUpdateInterval();
+    }
+
+    // Copy all donation-related functions from the original file
+    // Including: createMilestoneMarkers, fetchDonationAmount, updateDonationUI, etc.
+}
+
+const donationTracker = new DonationTracker(); 
